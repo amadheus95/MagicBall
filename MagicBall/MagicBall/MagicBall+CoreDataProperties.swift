fbc956233c04b18086927db9d9ade185819222ca
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-08-19 14:54:39 -0400

    Configurando o AppDelegate para CoreData.

---
+++ MagicBall/MagicBall/MagicBall+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  MagicBall+CoreDataProperties.swift
+//  MagicBall
+//
+//  Created by Rodrigo Machado on 8/19/15.
+//  Copyright © 2015 BEPiD. All rights reserved.
+//
+//  Delete this file and regenerate it using "Create NSManagedObject Subclass…"
+//  to keep your implementation up to date with your model.
+//
+
+import Foundation
+import CoreData
+
+extension MagicBall
+{
+    @NSManaged var ballNumber: NSNumber?
+    @NSManaged var arrayBalls: NSNumber?
+    @NSManaged var coins: NSNumber?
+
+}
---
+++ MagicBall/MagicBall/MagicBall.swift
@@ -0,0 +1,46 @@
+//
+//  MagicBall.swift
+//  MagicBall
+//
+//  Created by Rodrigo Machado on 8/19/15.
+//  Copyright © 2015 BEPiD. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class MagicBall: NSManagedObject
+{
+//    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+//    let managedContext = appDelegate.managedObjectContext
+//    let entity = NSEntityDescription.entityForName("Esportes", inManagedObjectContext: managedContext)
+//    let atribute = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+//    
+//    atribute.setValue(tfTime.text, forKey: "time")
+//    atribute.setValue(tfEsporte.text, forKey: "nomeEsporte")
+//    atribute.setValue(tfPais.text, forKey: "pais")
+//    atribute.setValue(tfCampeonato.text, forKey: "campeonato")
+//    
+//    var error: NSError?
+//    
+//    do
+//    {
+//    try managedContext.save()
+//    }
+//    
+//    catch let error2 as NSError
+//    {
+//    error = error2
+//    print("ERRO \(error), \(error?.userInfo)")
+//    }
+//    
+//    ListaTimesViewController.lista.append(atribute)
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    
+    
+    
+    
+}
